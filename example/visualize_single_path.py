7c09f26399fd4d4d962abc59c92a687d2a5fd436
Lzzya <lzzy201311@163.com>
2018-07-11 10:17:53 +0800

    测试用
    测试用添加

--- example/visualize_single_path.py
+++ example/visualize_single_path.py
@@ -6,3 +6,5 @@ import load_data as ldd
 warehouse, orders, charging, id_type_map = ldd.load_node_info("../data/input_node.csv")
 
 plot_one_path_liu(warehouse, orders, charging, [402,464,25,993,1053,587], id_type_map)
+
+print("END")
--- test.py
+++ test.py
@@ -1,6 +1,8 @@
 from algorithm.calculate import path_distance_time
 from algorithm.verify import if_path_legal
 import load_data as ldd
+from algorithm.verify import if_go_to_charge
+
 warehouse, orders, charging, id_type_map = ldd.load_node_info("data/input_node.csv")
 
 id_sorted_orders = sorted(orders, key = lambda x: x.id)
@@ -9,11 +11,18 @@ vehicles = ldd.load_vehicle_info('data/input_vehicle_type.csv')
 distance_matrix, time_matrix = ldd.load_distance_time_info('data/input_distance-time.csv')
 for o in orders:
     o.set_charging(charging, distance_matrix)
+#
+# test_path = [402,464,25,993,1053,587]
+# try_path = [633, 188]
+# t = path_distance_time(test_path,distance_matrix, time_matrix)
+# t2 = path_distance_time(try_path,distance_matrix, time_matrix)
+# t_if = if_path_legal(id_sorted_orders, try_path, distance_matrix, time_matrix, vehicles[1], id_type_map)
+# print("该路径是否合法",t_if)
+# print(t,t2)
 
-test_path = [402,464,25,993,1053,587]
-try_path = [25,402,464,1053,993,587]
-t = path_distance_time(test_path,distance_matrix, time_matrix)
-t2 = path_distance_time(try_path,distance_matrix, time_matrix)
-t_if = if_path_legal(id_sorted_orders, try_path, distance_matrix, time_matrix, vehicles[1], id_type_map)
-print("该路径是否合法",t_if)
-print(t,t2)
+# print(orders[822].charging_binding)
+# t = if_go_to_charge(id_sorted_orders, [822], distance_matrix, time_matrix, vehicles[1], id_type_map,0.3)
+
+
+t = if_path_legal(id_sorted_orders, [822], distance_matrix, time_matrix, vehicles[1], id_type_map)
+print(t)
